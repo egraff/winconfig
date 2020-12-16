16176d88f048fef2ac0aae7661a7e3a1fac4e836
Erlend Graff <erlend.graff@gmail.com>
2020-12-16 19:59:28 +0100

    Try to setup WinRM

--- run.ps1
+++ run.ps1
@@ -1,11 +1,32 @@
 try
 {
   cd $PSScriptRoot
+
+  Enable-PSRemoting -SkipNetworkProfileCheck
+  if ((Get-WSManInstance -ResourceURI winrm/config/service/auth).Negotiate -ne 'true')
+  {
+    Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WSMAN\Service -Name auth_negotiate -Value 1 -Type DWord
+    Restart-Service WinRM
+  }
+
+  $shouldResetUac = $false
+  $uacProperty = Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System | Select-Object -ExpandProperty LocalAccountTokenFilterPolicy
+  if ($uacProperty -ne 1)
+  {
+    $shouldResetUac = $true
+    Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name LocalAccountTokenFilterPolicy -Value 1 -Type DWord
+  }
+
   cd ansible
   & ..\msys64\usr\bin\sh.exe /run.sh
 }
 finally
 {
+  if ($shouldResetUac)
+  {
+    Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name LocalAccountTokenFilterPolicy -Value 0 -Type DWord
+  }
+
   Write-Host -NoNewLine 'Press any key to continue...';
   $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 }
